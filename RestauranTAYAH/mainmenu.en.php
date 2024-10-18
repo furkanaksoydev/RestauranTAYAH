e16ddf11eba4779a0e97446f005ba56d24ff53bf
Furkan Aksoy <123205618+FurkanKdzc@users.noreply.github.com>
2024-10-18 20:03:52 +0300

    RestauranTAYAH
    Site I created as projects

---
+++ RestauranTAYAH/mainmenu.en.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang"tr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="keywords" content="Sehayat Yolculuk Firması">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>RestautanTAYAH - Home Page</title>
+         
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/maainmenu.css">
+    </head>
+    
+
+    <header>
+        <div class="container-head">
+            <div class="logo">
+                <a href="mainmenu.php"><img src="resimler/logo.png"></a>
+            </div>
+            <div class="menu">
+                <u1>
+
+                    <li><a href="https://www.instagram.com/hayattoplu/"><img src="resimler/insta.png" width="100px;"></a></li>
+                    <li><a href="index.php">TR</a></li>
+                    <li><img src="resimler/cizgi.png" width="20px;"></li>
+                    <li class="secili"><a href="mainmenu.en.php">EN</a></li>
+
+                </u1>
+            </div>
+        </div>
+    </header>
+        
+        
+        
+    <body> 
+        <div class="section-area">
+            <section>
+                <div class="giris">
+                    <img src="resimler/logo.png" width="490px;">
+
+                    <br><br>
+
+                    <h2>Welcome</h2>
+                    <p>Address of delicious food</p>
+                </div>
+            </section>
+
+            <br><br><br>
+
+            <div class="bilgi">
+                <p>Our Bar</p>    
+            </div>
+
+            <br><br><br>
+
+            <section>
+                <div class="bar-tanitim">
+                    <div class="bar-resim">
+                        <img src="restoran/bar.jpg" width="700px;">
+                    </div>
+                    <div class="bar-genel">
+                        <p>Special Cocktails: Be sure to try the special cocktails prepared by our expert bartenders with fresh and quality ingredients.</p>
+                        <p>Exclusive Wines: Discover world wines with our rich wine menu and find the perfect harmony with your meal.</p>
+                        <p>Beer Types: We appeal to your taste with our extensive beer menu, including local and imported beers.</p>
+                    </div>
+                </div>
+            </section>
+
+            <br><br><br>
+
+            <div class="bilgi">
+                <p>Types of Food</p>    
+            </div>
+
+            <br><br><br>
+
+            <section>
+                <div class="menu">
+                    <h2>Appetizers (Starters)</h2>
+                    <div class="menu-item sol">
+                        <img src="restoran/baslangic.jpg" width="700px;" height="500px;" alt="Başlangıçlar">
+                        <div class="overlay">Appetizers are options that help you start your meal with a delightful beginning, consisting of salads, mezes, and light snacks.</div>
+                    </div>
+                    <h2 class="sag">Warm Appetizers (Hot Starters)</h2>
+                    <div class="menu-item sag">
+                        <img src="restoran/arasicak.jpg" width="700px;" height="500px;" alt="Ara Sıcaklar">
+                        <div class="overlay">Warm appetizers are tasty options served hot and are usually shared before the main course, offering filling and delicious bites.</div>
+                    </div>
+                    <h2>Main Courses</h2>
+                    <div class="menu-item sol">
+                        <img src="restoran/anayemek.jpg" width="700px;" height="500px;" alt="Ana Yemekler">
+                        <div class="overlay">Main courses make up the largest part of the menu and typically include primary meal options such as meat, fish, chicken, or vegetarian dishes.</div>
+                    </div>
+                    <h2 class="sag">Desserts</h2>                
+                    <div class="menu-item sag">
+                        <img src="restoran/tatli.jpg" width="700px;" height="500px;" alt="Tatlılar">
+                        <div class="overlay">Desserts will brighten your day with sweet flavors at the end of your meal, offering various cakes, ice creams, dessert plates, or fruit platters.</div>
+                    </div>
+                    <h2>Beverages</h2>
+                    <div class="menu-item sol">
+                        <img src="restoran/icecek.jpg" width="700px;" height="500px;" alt="İçecekler">
+                        <div class="overlay">In the beverages section, you can choose from a range of alcoholic or non-alcoholic drinks such as wine, beer, soft drinks, tea, or coffee to complement your meal with tasty options.</div>
+                    </div>
+                </div>
+            </section>
+            
+            <div class="bilgi">
+                <p>Menu</p>    
+            </div>
+
+            <br><br><br>
+            
+            <div class="bilgi">
+                <p>Location</p>    
+            </div>
+
+            <br><br><br>
+            
+            <section class="konum">
+                    <div class="adres">
+                        <h3>Adress</h3>
+                        <h4>Bira Fabrikası</h4>
+                        <p>Rumeli Hisarı</p>
+                        <p>Hisar Üstü Nispetiye Cd</p> 
+                        <p>81 A</p>
+                        <p>34470 Sarıyer/İstanbul</p>
+                        <p>Phone Number : 05319049663</p>
+                    </div>
+                    <div class="nasıl Giderim">
+                        <h3>Public Transport</h3>
+                        <b>Bus</b> : 43R, 599C, 59R, 59RK, 59RS<br>
+                        <b>Metro</b> : M6, F4<br>
+                        <b>Minibus</b> : ETILER - BEŞIKTAŞ, <br>
+                        BALTALIMANI-ETILER-NISPETIYE-MECIDIYEKÖY, BALTALIMANI - 4. LEVENT
+                    </div>
+            </section>
+        </div>
+        
+    </body>
+</html>
